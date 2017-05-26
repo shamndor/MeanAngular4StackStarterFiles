1114d4eb866ddd40765bee2dfc6d9079e1f1dd04
shamndor <ahmed.mahmod85@hotmail.com>
2017-05-26 18:23:44 +0200

    first commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+ 
+
+import 'rxjs/RX';
+@Component({
+
+    selector: 'pm-app',
+    template: `
+     <div>
+         <h5> Welcom in MEAN Stack 4 World , have a fan mate </h5>
+     </div>
+    `,
+   
+
+
+})
+export class AppComponent { }
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent }  from './app.component'; 
+import { HttpModule } from '@angular/http'; 
+import {RouterModule} from '@angular/router'
+ 
+@NgModule({
+  imports: [ 
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  
+    
+    ],
+  declarations: [ AppComponent],
+  bootstrap: [ AppComponent ],
+   
+})
+export class AppModule { }
